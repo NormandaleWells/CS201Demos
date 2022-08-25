35d7496b03eaf1a4982aeb6ffa5205cdbae8dab2
Richard Wells <rwells@carleton.edu>
2022-08-25 16:17:44 -0500

    Renamed to snake case; changed presentation order.
    Decided to introduct main() before if __name__.

--- PythonToJava/hello_world_2.py
+++ PythonToJava/hello_world_2.py
@@ -1,3 +1,6 @@
 
-if __name__ == "__main__":
+def main():
     print("Hello, world!")
+
+
+main()
--- PythonToJava/hello_world_3.py
+++ PythonToJava/hello_world_3.py
@@ -2,5 +2,6 @@
 def main():
     print("Hello, world!")
 
+
 if __name__ == "__main__":
     main()
