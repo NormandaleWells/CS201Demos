da9249fd9d9a7edae830a4ac7d517410b3748896
Richard Wells <rwells@carleton.edu>
2022-08-25 16:21:51 -0500

    New type checking demonstration.

---
+++ PythonToJava/add.java
@@ -0,0 +1,15 @@
+public class add {
+
+    public static int add2(int a, int b) {
+        return a + b;
+    }
+ 
+    public static String add2(String a, String b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(add2(14, 28));
+        System.out.println(add2("forty ", "two"));
+    }
+}
---
+++ PythonToJava/add.py
@@ -0,0 +1,12 @@
+
+def add(a, b):
+    return a + b
+
+
+def main():
+    print(add(14, 28))
+    print(add("forty ", "two"))
+
+
+if __name__ == "__main__":
+    main()
