4c4f043531239ae40d1d60e80ef92118b822156d
Richard Wells <Richard.Wells@normandale.edu>
2022-10-09 21:00:53 -0500

    Finished genericizing ArrayUtils.

--- GenericSet/ArrayUtils.java
+++ GenericSet/ArrayUtils.java
@@ -105,74 +105,74 @@ public final class ArrayUtils {
 		return maxElement(a, 0, a.length);
 	}
 
-	// // swap swaps the values at indices 'idx1' and 'idx2' in
-	// // the array 'a'.
-	// public static void swap(int[] a, int idx1, int idx2) {
-	// 	checkArguments(a, idx1, idx2, true);
-	// 	int t = a[idx1];
-	// 	a[idx1] = a[idx2];
-	// 	a[idx2] = t;
-	// }
+	// swap swaps the values at indices 'idx1' and 'idx2' in
+	// the array 'a'.
+	public static <T> void swap(T[] a, int idx1, int idx2) {
+		checkArguments(a, idx1, idx2, true);
+		T t = a[idx1];
+		a[idx1] = a[idx2];
+		a[idx2] = t;
+	}
 
-	// // copy copies the subrange aFrom[lo,hi) to aTo[lo,hi).  All
-	// // other elements of aTo are untouched.
-	// public static void copy(int[] aFrom, int[] aTo, int lo, int hi) {
-	// 	checkArguments(aFrom, lo, hi, true);
-	// 	checkArguments(aTo, lo, hi, true);
-	// 	for (int i = lo; i < hi; i++)
-	// 		aTo[i] = aFrom[i];
-	// }
+	// copy copies the subrange aFrom[lo,hi) to aTo[lo,hi).  All
+	// other elements of aTo are untouched.
+	public static <T> void copy(T[] aFrom, T[] aTo, int lo, int hi) {
+		checkArguments(aFrom, lo, hi, true);
+		checkArguments(aTo, lo, hi, true);
+		for (int i = lo; i < hi; i++)
+			aTo[i] = aFrom[i];
+	}
 
-	// // copy copies the subrange aFrom[loFrom,hiFrom) to
-	// // aTo[aTo,aTo+(hiFrom-loFrom)).  All other elements
-	// // of aTo are untouched.
-	// public static void copy(int[] aFrom, int loFrom, int hiFrom, int[] aTo, int loTo) {
-	// 	checkArguments(aFrom, loFrom, hiFrom, true);
-	// 	checkArguments(aTo, loTo, loTo + (hiFrom-loFrom), true);
-	// 	for (int iFrom = loFrom, iTo = loTo; iFrom < hiFrom; iFrom++, iTo++) {
-	// 		aTo[iTo] = aFrom[iFrom];
-	// 	}
-	// }
+	// copy copies the subrange aFrom[loFrom,hiFrom) to
+	// aTo[aTo,aTo+(hiFrom-loFrom)).  All other elements
+	// of aTo are untouched.
+	public static <T> void copy(T[] aFrom, int loFrom, int hiFrom, T[] aTo, int loTo) {
+		checkArguments(aFrom, loFrom, hiFrom, true);
+		checkArguments(aTo, loTo, loTo + (hiFrom-loFrom), true);
+		for (int iFrom = loFrom, iTo = loTo; iFrom < hiFrom; iFrom++, iTo++) {
+			aTo[iTo] = aFrom[iFrom];
+		}
+	}
 
-	// // rotateRight() rotates all elements of a[lo,hi) one position
-	// // to the right.
-	// public static void rotateRight(int[] a, int lo, int hi) {
-	// 	checkArguments(a, lo, hi, false);
-	// 	if (hi - lo < 2) return;
-	// 	int t = a[hi-1];
-	// 	for (int i = hi-2; i >= lo; i--)
-	// 		a[i+1] = a[i];
-	// 	a[lo] = t;
-	// }
+	// rotateRight() rotates all elements of a[lo,hi) one position
+	// to the right.
+	public static <T> void rotateRight(T[] a, int lo, int hi) {
+		checkArguments(a, lo, hi, false);
+		if (hi - lo < 2) return;
+		T t = a[hi-1];
+		for (int i = hi-2; i >= lo; i--)
+			a[i+1] = a[i];
+		a[lo] = t;
+	}
 
-	// // rotateLeft() rotates all elements of a[lo,hi) one position
-	// // to the left.
-	// public static void rotateLeft(int[] a, int lo, int hi) {
-	// 	checkArguments(a, lo, hi, false);
-	// 	if (hi - lo < 2) return;
-	// 	int t = a[lo];
-	// 	for (int i = lo+1; i < hi; i++)
-	// 		a[i-1] = a[i];
-	// 	a[hi-1] = t;
-	// }
+	// rotateLeft() rotates all elements of a[lo,hi) one position
+	// to the left.
+	public static <T> void rotateLeft(T[] a, int lo, int hi) {
+		checkArguments(a, lo, hi, false);
+		if (hi - lo < 2) return;
+		T t = a[lo];
+		for (int i = lo+1; i < hi; i++)
+			a[i-1] = a[i];
+		a[hi-1] = t;
+	}
 
-	// public static void main(String[] args) {
+	public static void main(String[] args) {
 		
-	// 	int i = 0;
-	// 	assert i++ == 0;
-	// 	if (i != 1) {
-	// 		System.out.println("This needs to be run with -ea.\n");
-	// 		return;
-	// 	}
+		int i = 0;
+		assert i++ == 0;
+		if (i != 1) {
+			System.out.println("This needs to be run with -ea.\n");
+			return;
+		}
 
-	// 	int[] a1 = { 5, 3, 7, 11, 2 };
-	// 	int[] a2 = { 2, 3, 3, 5, 7, 7, 7, 11 };
+		Integer[] a1 = { 5, 3, 7, 11, 2 };
+		Integer[] a2 = { 2, 3, 3, 5, 7, 7, 7, 11 };
 		
-	// 	// TODO: write a real JUnit test suite for these
-	// 	assert find(a1, 11) ==3;
-	// 	assert find(a1, 4) == -1;
-	// 	assert count(a2, 7) == 3;
-	// 	assert minElement(a1, 1, 5) == 4;
-	// 	assert maxElement(a1, 1, 5) == 3;
-	// }
+		// TODO: write a real JUnit test suite for these
+		assert find(a1, 11) ==3;
+		assert find(a1, 4) == -1;
+		assert count(a2, 7) == 3;
+		assert minElement(a1, 1, 5) == 4;
+		assert maxElement(a1, 1, 5) == 3;
+	}
 }
